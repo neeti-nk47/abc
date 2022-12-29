e246e766e9610ec26da16e4ab814330ca91f695a
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-30 01:26:03 +0530

    added js to this

---
+++ main.js
@@ -0,0 +1,8 @@
+document.title= "New Title";
+
+let HeaderTitle= document.getElementById("main-header");
+let AddItemstag= document.getElementsByClassName("title")[0];
+
+HeaderTitle.style.border="solid 3px Black";
+AddItemstag.innerHTML="<strong>Add Items</strong>"
+AddItemstag.style.color="green"
