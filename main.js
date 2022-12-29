83d37323712d74acb0f017907e355bc67a2229d6
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-30 02:24:09 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,29 @@
-document.title= "New Title";
+// document.title= "New Title";
 
-let HeaderTitle= document.getElementById("main-header");
-let AddItemstag= document.getElementsByClassName("title")[0];
-let items= document.getElementsByClassName("list-group-item");
+// let HeaderTitle= document.getElementById("main-header");
+// let AddItemstag= document.getElementsByClassName("title")[0];
+// let items= document.getElementsByClassName("list-group-item");
 
-HeaderTitle.style.border="solid 3px Black";
-AddItemstag.style.fontWeight="bold";
-AddItemstag.style.color="green";
-items[2].style.background="green";
-for (let i=0; i<items.length; i++){
-    items[i].style.fontWeight="bold";
+// HeaderTitle.style.border="solid 3px Black";
+// AddItemstag.style.fontWeight="bold";
+// AddItemstag.style.color="green";
+// items[2].style.background="green";
+// for (let i=0; i<items.length; i++){
+//     items[i].style.fontWeight="bold";
+// }
+
+// let li= document.getElementsByTagName("li");
+// li[4].style.background="green";
+
+// let secondItem= document.querySelector("li:nth-child(2)")
+// secondItem.style.background="green";
+// let thirdItem= document.querySelector("li:nth-child(3)")
+// thirdItem.style.color="transparent";
+
+let list= document.querySelectorAll("li");
+list[1].style.color="green";
+
+let oddlist= document.querySelectorAll("li:nth-child(odd)");
+for(let i=0; i<oddlist.length; i++){
+    oddlist[i].style.background="green";
 }
-
-let li= document.getElementsByTagName("li");
-li[4].style.background="green";
