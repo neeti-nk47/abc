4edcc25155e63d9c6aa05f637df4320946284244
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-30 01:52:54 +0530

    Update main.js

--- main.js
+++ main.js
@@ -5,9 +5,12 @@ let AddItemstag= document.getElementsByClassName("title")[0];
 let items= document.getElementsByClassName("list-group-item");
 
 HeaderTitle.style.border="solid 3px Black";
-AddItemstag.innerHTML="<strong>Add Items</strong>"
+AddItemstag.style.fontWeight="bold";
 AddItemstag.style.color="green";
 items[2].style.background="green";
 for (let i=0; i<items.length; i++){
     items[i].style.fontWeight="bold";
 }
+
+let li= document.getElementsByTagName("li");
+li[4].style.background="green";
