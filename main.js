8190d9778c6c4cddf8511a3cb53c53af20c0b5a1
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-30 01:43:57 +0530

    some minor changes in dom

--- main.js
+++ main.js
@@ -2,7 +2,12 @@ document.title= "New Title";
 
 let HeaderTitle= document.getElementById("main-header");
 let AddItemstag= document.getElementsByClassName("title")[0];
+let items= document.getElementsByClassName("list-group-item");
 
 HeaderTitle.style.border="solid 3px Black";
 AddItemstag.innerHTML="<strong>Add Items</strong>"
-AddItemstag.style.color="green"
+AddItemstag.style.color="green";
+items[2].style.background="green";
+for (let i=0; i<items.length; i++){
+    items[i].style.fontWeight="bold";
+}
