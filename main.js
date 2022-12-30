e0337a682fc08202b2989b68f76483e1cf454b78
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-31 01:22:43 +0530

    lister updates

--- main.js
+++ main.js
@@ -1,29 +1,39 @@
-// document.title= "New Title";
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
-// let HeaderTitle= document.getElementById("main-header");
-// let AddItemstag= document.getElementsByClassName("title")[0];
-// let items= document.getElementsByClassName("list-group-item");
 
-// HeaderTitle.style.border="solid 3px Black";
-// AddItemstag.style.fontWeight="bold";
-// AddItemstag.style.color="green";
-// items[2].style.background="green";
-// for (let i=0; i<items.length; i++){
-//     items[i].style.fontWeight="bold";
-// }
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
 
-// let li= document.getElementsByTagName("li");
-// li[4].style.background="green";
 
-// let secondItem= document.querySelector("li:nth-child(2)")
-// secondItem.style.background="green";
-// let thirdItem= document.querySelector("li:nth-child(3)")
-// thirdItem.style.color="transparent";
+function addItem(e){
+  e.preventDefault();  
+  var newItem = document.getElementById('item').value;
 
-let list= document.querySelectorAll("li");
-list[1].style.color="green";
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
 
-let oddlist= document.querySelectorAll("li:nth-child(odd)");
-for(let i=0; i<oddlist.length; i++){
-    oddlist[i].style.background="green";
+  var deleteBtn = document.createElement('button');
+
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  
+  itemList.appendChild(li);
 }
+
+
+function removeItem(e){
+
+  if(e.target.classList.contains('delete')){
+
+    if(confirm('Are You Sure?')){
+
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+
