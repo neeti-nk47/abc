df1e8833ec52eda8a1f756f88f39f7e3fea16504
neeti-nk47 <121576070+neeti-nk47@users.noreply.github.com>
2022-12-31 02:00:43 +0530

    search bar

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 
 form.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', filterItems);
+
 
 
 function addItem(e){
   e.preventDefault();  
   var newItem = document.getElementById('item').value;
+  
 
   var li = document.createElement('li');
   li.className = 'list-group-item';
@@ -35,5 +39,22 @@ function removeItem(e){
     }
   }
 }
+
+function filterItems(e){    
+    var text = e.target.value.toLowerCase();
+    var items = itemList.getElementsByTagName('li');
+    
+    Array.from(items).forEach(function(item){
+      var itemName = item.firstChild.textContent;
+
+      if(itemName.toLowerCase().indexOf(text) != -1){
+        item.style.display = 'block';
+      } 
+      else {
+        item.style.display = 'none';
+      }
+    });
+  }
+
 
 
